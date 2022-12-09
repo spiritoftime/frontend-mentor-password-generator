b55111f0b432011230c1ddf186b72017dd8bf1a7
spiritoftime <chooliangyi6@gmail.com>
2022-12-09 14:26:41 +0800

    reflect changes in production build

--- my-app/src/components/GenerateButton.js
+++ my-app/src/components/GenerateButton.js
@@ -12,20 +12,19 @@ const GenerateButton = (props) => {
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
-
-        GENERATE AGAIN
-        <svg
-          className={classes.icon}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 11.1 12"
-        >
-          <path
-            fill={!isHover ? "#24232C" : "#a4ffaf"}
-            d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"
-          ></path>
-        </svg>
-
+      GENERATE AGAIN
+      <svg
+        className={classes.icon}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 11.1 12"
+      >
+        <path
+          fill={!isHover ? "#24232C" : "#a4ffaf"}
+          d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"
+        ></path>
+      </svg>
     </button>
+    // testing
   );
 };
 
